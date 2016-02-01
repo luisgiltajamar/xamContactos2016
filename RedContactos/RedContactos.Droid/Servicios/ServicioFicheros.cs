e8f7ae211dddf09a2d5dbe99a704d8c640610059
Luis Felipe Gil Lamaignere <luis.gil@toolents.com>
2016-02-01 10:44:07 +0100

    a

---
+++ RedContactos/RedContactos.Droid/Servicios/ServicioFicheros.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.IO;
+using RedContactos.Droid.Servicios;
+using RedContactos.Service;
+using Xamarin.Forms;
+
+
+[assembly: Dependency(typeof(ServicioFicheros))]
+namespace RedContactos.Droid.Servicios
+{
+    public class ServicioFicheros:IServicioFicheros
+    {
+        public void GuardarTexto(string texto, string fichero)
+        {
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var rutafinal = Path.Combine(path, fichero);
+            File.WriteAllText(rutafinal,texto);
+        }
+
+        public string RecuperarTexto(string fichero)
+        {
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var rutafinal = Path.Combine(path, fichero);
+            try
+            {
+                return File.ReadAllText(rutafinal);
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+    }
+}
